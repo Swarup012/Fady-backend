64ae2a530d036665c47cfe2c66141baa34703947
swarup <swarupbasu325@gmail.com>
2026-01-14 17:56:01 +0530

    update

--- src/app.js
+++ src/app.js
@@ -158,7 +158,7 @@ app.use((req, res) => {
   res.status(404).json({ success: false, error: "Route not found" });
 });
 
-// Error Handlers
+// Error Handler
 const { errorHandler } = require("./middleware/error.middleware");
 app.use(errorHandler);
 
